6c7eba69b7b2c1055982798634f8a4ec3b4dcb0a
ViticoMC <victormanuelmartinezcampo178@gmail.com>
2025-09-29 01:00:05 -0400

    changue player position

--- canon.gd
+++ canon.gd
@@ -1,15 +1,18 @@
 extends Node2D
 const balas =preload("res://escenas/balas.tscn")
 
+var player
+func _ready() -> void:
+	player = $"../..".get_child(0)
+
 func disparar():
 	var bala = balas.instantiate()
 	bala.apply_central_impulse(Vector2(-700, -300))
 	bala.global_position = $Marker2D.global_position
 	$"..".add_child(bala)
 
-
 func _on_timer_timeout() -> void:
-	if $"../player"!=null:
+	if player!=null:
 		disparar()
 	else:
 		$Timer.stop()
